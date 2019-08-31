b819a34919bd46068f7c27189e6879eb11fb1cbf
Joe D'Amore <joe@joedamore.me>
2019-08-31 12:20:20 -0400

    Add default gem files

---
+++ lib/vagrant/wizard.rb
@@ -0,0 +1,8 @@
+require "vagrant/wizard/version"
+
+module Vagrant
+  module Wizard
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/wizard/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Wizard
+    VERSION = "0.1.0"
+  end
+end
